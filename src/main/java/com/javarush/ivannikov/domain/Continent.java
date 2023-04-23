07c417159ef5875c4b5ac56a22843b6fdc28f82a
iceivannikov <iceivannikov@gmail.com>
2023-04-23 19:54:47 +0300

    Initial commit

---
+++ src/main/java/com/javarush/ivannikov/domain/Continent.java
@@ -0,0 +1,12 @@
+package com.javarush.ivannikov.domain;
+
+public enum Continent {
+        ASIA,
+        EUROPE,
+        NORTH_AMERICA,
+        AFRICA,
+        OCEANIA,
+        ANTARCTICA,
+        SOUTH_AMERICA
+
+}
